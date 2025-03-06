c80c06a85dd477e24bf142868dc8a83de4593ee0
Anettyyy <aneta.maurencova@student.spsmb.cz>
2025-03-06 09:06:53 +0100

    upd

---
+++ res/js/main.js
@@ -0,0 +1,70 @@
+class Player {
+  constructor(x, y, w, h, c) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+    this.c = c;
+    this.v = 5;
+    //velocity
+  }
+
+  draw(ctx) {
+    ctx.fillStyle = this.c;
+    ctx.fillRect(this.x, this.y, this.w, this.h);
+  }
+
+  update(keys) {
+    if (keys["w"]) {
+      if (this.y - this.v > 0){
+        this.y -= this.v;
+      }
+    }
+    if (keys["s"]) {
+      if (this.y + this.h + this.v < gameCanvas.height){
+        this.y += this.v;
+      }
+    }
+    if (keys["a"]) {
+      if (this.x - this.v > 0){
+        this.x -= this.v;
+      }
+    }
+    if (keys["d"]) {
+      if (this.x + this.w + this.v < gameCanvas.width){
+        this.x += this.v;
+      }
+    }
+  }
+}
+
+const myPlayer = new Player(10, 10, 50, 50, "red");
+
+const gameCanvas = document.getElementById("gameCanvas");
+const ctx = gameCanvas.getContext("2d");
+
+const resizeCanvas = () => {
+  gameCanvas.width = window.innerWidth;
+  gameCanvas.height = window.innerHeight;
+};
+
+window.addEventListener("resize", resizeCanvas);
+resizeCanvas();
+
+let keys = {};
+
+//                             e.key = w, do keys se prida w: true
+window.addEventListener("keydown", (e) => (keys[e.key] = true));
+window.addEventListener("keyup", (e) => (keys[e.key] = false));
+
+const gameLoop = () => {
+  ctx.clearRect(0, 0, gameCanvas.width, gameCanvas.height);
+
+  //update
+  myPlayer.update(keys);
+  //draw
+  myPlayer.draw(ctx);
+  requestAnimationFrame(gameLoop);
+};
+
+requestAnimationFrame(gameLoop);
